c6041cd8b42b1bf24542a142cd6f316a4437ffdf
lalit3c <lalitchaudhary0115@gmail.com>
2026-03-09 23:19:38 +0100

    Add upload script for concept pairs to Hugging Face repository

---
+++ utils/upload_to_hf.py
@@ -0,0 +1,24 @@
+from huggingface_hub import HfApi, hf_hub_download, list_repo_files
+import os
+HF_REPO_ID = "lalit3c/OA_Domain_Concepts"
+HF_TOKEN = os.getenv("HF_TOKEN")
+assert HF_TOKEN, "HF_TOKEN environment variable not set"
+
+api = HfApi()
+
+
+# configuration
+local_file = "results/concept_pairs_first_occurence.parquet"             # file on your machine
+path_in_repo = "data/datconcept_pairs_first_occurencea.parquet"      # where it will appear in the dataset repo
+
+api = HfApi()
+
+api.upload_file(
+    path_or_fileobj=local_file,
+    path_in_repo=path_in_repo,
+    repo_id=HF_REPO_ID,
+    token = HF_TOKEN,
+    repo_type="dataset"
+)
+
+print("Upload complete.")
